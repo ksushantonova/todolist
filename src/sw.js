f898d868a56a636b7240f970e3506d214510265c
Ksu <msksushantonova@gmail.com>
2017-08-01 14:14:56 +0300

    sw

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 console.log('aaa');
 
-this.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
