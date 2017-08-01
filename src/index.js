eb2f138b53b6184d93b53fb660026f70a81a7304
Ksu <msksushantonova@gmail.com>
2017-08-01 13:43:46 +0300

    sw

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 // - при загрузке страницы проверять наличие сохраненных данных и строить по ним ToDoList с айтемами, если данных нет, то выводить только один пустой ToDoList (edited)
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/index.js', { scope: '/sw-test/' }).then(function(reg) {
+  navigator.serviceWorker.register('/sw-test/src/sw.js', { scope: '/sw-test/' }).then(function(reg) {
     // регистрация сработала
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
@@ -13,8 +13,6 @@ if ('serviceWorker' in navigator) {
     console.log('Registration failed with ' + error);
   });
 }
-
-
 
 let flexed = document.getElementById('flexed');
 new BuildItem(flexed);
