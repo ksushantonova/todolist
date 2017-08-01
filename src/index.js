16ebaa2dae7ce5f4dff27e11333b3ee985f02f06
Ksu <msksushantonova@gmail.com>
2017-08-01 13:02:44 +0300

    serviseworker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 // - при загрузке страницы проверять наличие сохраненных данных и строить по ним ToDoList с айтемами, если данных нет, то выводить только один пустой ToDoList (edited)
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('https://ksushantonova.github.io', { scope: '/sw-test/' }).then(function(reg) {
+  navigator.serviceWorker.register('https://ksushantonova.github.io/sw-test/', { scope: '/sw-test/' }).then(function(reg) {
     // регистрация сработала
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
