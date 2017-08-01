2f91de5151e7a8a1383fc22c77b94c7c0ce441b9
Ksu <msksushantonova@gmail.com>
2017-08-01 14:42:05 +0300

    dd

--- src/sw.js
+++ src/sw.js
@@ -1,20 +1,20 @@
 console.log('aaa');
 
-// self.addEventListener('install', function(event) {
-//   event.waitUntil(
-//     caches.open('v1').then(function(cache) {
-//       return cache.addAll([
-//         '/sw-test/',
-//         '/sw-test/index.html',
-//         '/sw-test/index3.css',
-//         '/sw-test/todolist.js',
-//         '/sw-test/todolistitem.js',
-//         '/sw-test/builditem.js',
-//         '/sw-test/all2.png',
-//         '/sw-test/del2.png',
-//         '/sw-test/cross.png',
-//         '/sw-test/index.js'
-//       ]);
-//     })
-//   );
-// });
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/sw-test/',
+        '/sw-test/index.html',
+        '/sw-test/index3.css',
+        '/sw-test/todolist.js',
+        '/sw-test/todolistitem.js',
+        '/sw-test/builditem.js',
+        '/sw-test/gallery/all2.png',
+        '/sw-test/gallery/del2.png',
+        '/sw-test/gallery/cross.png',
+        '/sw-test/gallery/index.js'
+      ]);
+    })
+  );
+});
