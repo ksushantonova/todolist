8f79a8122bafc229164ca0b65b5880b5b6e35746
Ksu <msksushantonova@gmail.com>
2017-08-01 14:58:35 +0300

    d

--- sw.js
+++ sw.js
@@ -1,19 +1,16 @@
-console.log('aaa');
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/index3.css',
-        '/sw-test/todolist.js',
-        '/sw-test/todolistitem.js',
-        '/sw-test/builditem.js',
-        '/sw-test/gallery/all2.png',
-        '/sw-test/gallery/del2.png',
-        '/sw-test/gallery/cross.png',
-        '/sw-test/gallery/index.js'
+        'index.html',
+        'index3.css',
+        'src/todolist.js',
+        'src/todolistitem.js',
+        'src/builditem.js',
+        'src/gallery/all2.png',
+        'src/gallery/del2.png',
+        'src/gallery/cross.png',
+        'src/index.js'
       ]);
     })
   );
