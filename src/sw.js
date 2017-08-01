b976455db2100d726b0c0502ec492c3507bf53ce
Ksu <msksushantonova@gmail.com>
2017-08-01 14:48:32 +0300

    dd

---
+++ src/sw.js
@@ -0,0 +1,19 @@
+console.log('aaa');
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'index3.css',
+        'src/todolist.js',
+        'src/todolistitem.js',
+        'src/builditem.js',
+        'src/gallery/all2.png',
+        'src/gallery/del2.png',
+        'src/gallery/cross.png',
+        'src/index.js'
+      ]);
+    })
+  );
+});
